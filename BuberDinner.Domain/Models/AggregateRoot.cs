89cb76208a8b6115c59d68ac12c7a1aea490cd22
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-01-12 09:46:21 +0700

    Implement aggregate root, entity, value object

---
+++ BuberDinner.Domain/Models/AggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BuberDinner.Domain.Models
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id)
+        {
+        }
+    }
+}
+
